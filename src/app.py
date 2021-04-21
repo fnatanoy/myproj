6b3bbfd441345a29b36a148bb8eadbf57bc9bfed
kenshoo <fnatanoy@gmail.com>
2021-04-21 15:59:22 +0300

    wip

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from commonlib.commonlib import greeting
 def run():
     greeting.greeting("Git Submodule")
     greeting.greeting_2("Git Submodule")
+    greeting.greeting_3("Git Submodule")
 
 if __name__ == "__main__":
     run()
